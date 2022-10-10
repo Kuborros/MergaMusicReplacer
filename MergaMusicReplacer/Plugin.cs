3997e774c5bd07ac82106234f29a237b22db20d7
AdmiralDock <mmodri@outlook.com>
2022-10-10 20:18:28 +0200

    Added audio file extension check, some checks for config file formatting and support for TRACKER FILES! :O

--- MergaMusicReplacer/Plugin.cs
+++ MergaMusicReplacer/Plugin.cs
@@ -4,12 +4,14 @@ using HarmonyLib;
 using MergaMusicReplacer;
 using System;
 using System.IO;
+using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 using UnityEngine;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace MergaMusicReplacer
 {
-    [BepInPlugin("com.kuborro.plugins.fp2.mergamusic", "MergaMusicReplacer", "1.0.0")]
+    [BepInPlugin("com.kuborro.plugins.fp2.mergamusic", "MergaMusicReplacer", "1.1.0")]
     public class Plugin : BaseUnityPlugin
     {
         public static ConfigEntry<string> audioMerga1;
@@ -44,14 +46,25 @@ namespace MergaMusicReplacer
                 uri.Insert(0, "file:///");
             return uri.ToString();
         }
-
+        public static AudioType getAudioType(string extension)
+        {
+            if (extension == ".mp3") return AudioType.MPEG;
+            if (extension == ".wav") return AudioType.WAV;
+            if (extension == ".ogg") return AudioType.OGGVORBIS;
+            if (extension == ".s3m") return AudioType.S3M;
+            if (extension == ".mod") return AudioType.MOD;
+            if (extension == ".it") return AudioType.IT;
+            if (extension == ".xm") return AudioType.XM;
+            if (extension == ".aiff" || extension == ".aif") return AudioType.AIFF;
+            return AudioType.UNKNOWN;
+        }
         private void Awake()
         {
             var harmony = new Harmony("com.kuborro.plugins.fp2.mergamusic");
 
-            audioMerga1 = Config.Bind("General", "Phase 1", "M_Boss_Merga1", "Filename of the track for phase 1 (Blue/Blood/Super Moon).");
-            audioMerga2 = Config.Bind("General", "Phase 2", "M_Boss_Merga2", "Filename of the track for phase 2 (Eclipse/Lilith).");
-            audioMerga3 = Config.Bind("General", "Phase 3", "M_Boss_Merga3", "Filename of the track for phase 3 (Final).");
+            audioMerga1 = Config.Bind("General", "Phase 1", "M_Boss_Merga1.wav", "Filename of the track for phase 1 (Blue/Blood/Super Moon).");
+            audioMerga2 = Config.Bind("General", "Phase 2", "M_Boss_Merga2.wav", "Filename of the track for phase 2 (Eclipse/Lilith).");
+            audioMerga3 = Config.Bind("General", "Phase 3", "M_Boss_Merga3.wav", "Filename of the track for phase 3 (Final).");
             
             harmony.PatchAll(typeof(Patch));
 
@@ -68,39 +81,80 @@ class Patch
         if (bgmMusic == null) return;
         if (bgmMusic.name == "M_Boss_Merga1")
         {
-            string trackPath = Plugin.texPath + "\\" + Plugin.audioMerga1.Value + ".wav";
-            WWW audioLoader = new(Plugin.FilePathToFileUrl(trackPath));
-            AudioClip selectedClip = audioLoader.GetAudioClip(false, true, AudioType.WAV);
-            while (!(selectedClip.loadState == AudioDataLoadState.Loaded))
+            string trackPath = Plugin.texPath + "\\" + Plugin.audioMerga1.Value;
+            string ext = Path.GetExtension(trackPath);
+            bool stream = true;
+            if (ext == "")
             {
-                int i = 1;
+                ext = ".wav";
+                trackPath = trackPath + ext;
             }
-            selectedClip.name = bgmMusic.name;
-            bgmMusic = selectedClip;
+            if (ext is ".mod" or ".s3m" or ".it" or ".xm")
+            {
+                stream = false;
+            }
+            if (File.Exists(trackPath) && Plugin.getAudioType(ext) != AudioType.UNKNOWN) { 
+                WWW audioLoader = new(Plugin.FilePathToFileUrl(trackPath));
+                AudioClip selectedClip = audioLoader.GetAudioClip(false, stream, Plugin.getAudioType(ext));
+                while (!(selectedClip.loadState == AudioDataLoadState.Loaded))
+                {
+                    int i = 1; //we gotta wait for the file to load this is why this is here lol.
+                }
+                selectedClip.name = bgmMusic.name;
+                bgmMusic = selectedClip;
+            }
         }
         if (bgmMusic.name == "M_Boss_Merga2")
         {
-            string trackPath = Plugin.texPath + "\\" + Plugin.audioMerga2.Value + ".wav";
-            WWW audioLoader = new(Plugin.FilePathToFileUrl(trackPath));
-            AudioClip selectedClip = audioLoader.GetAudioClip(false, true, AudioType.WAV);
-            while (!(selectedClip.loadState == AudioDataLoadState.Loaded))
+            string trackPath = Plugin.texPath + "\\" + Plugin.audioMerga2.Value;
+            string ext = Path.GetExtension(trackPath);
+            bool stream = true;
+            if (ext == "")
             {
-                int i = 1;
+                ext = ".wav";
+                trackPath = trackPath + ext;
             }
-            selectedClip.name = bgmMusic.name;
-            bgmMusic = selectedClip; 
+            if (ext is ".mod" or ".s3m" or ".it" or ".xm")
+            {
+                stream = false;
+            }
+            if (File.Exists(trackPath) && Plugin.getAudioType(ext) != AudioType.UNKNOWN)
+            {
+                WWW audioLoader = new(Plugin.FilePathToFileUrl(trackPath));
+                AudioClip selectedClip = audioLoader.GetAudioClip(false, stream, Plugin.getAudioType(ext));
+                while (!(selectedClip.loadState == AudioDataLoadState.Loaded))
+                {
+                    int i = 1;
+                }
+                selectedClip.name = bgmMusic.name;
+                bgmMusic = selectedClip;
+            }
         }
         if (bgmMusic.name == "M_Boss_Merga3")
         {
-            string trackPath = Plugin.texPath + "\\" + Plugin.audioMerga3.Value + ".wav";
-            WWW audioLoader = new(Plugin.FilePathToFileUrl(trackPath));
-            AudioClip selectedClip = audioLoader.GetAudioClip(false, true, AudioType.WAV);
-            while (!(selectedClip.loadState == AudioDataLoadState.Loaded))
+            string trackPath = Plugin.texPath + "\\" + Plugin.audioMerga3.Value;
+            string ext = Path.GetExtension(trackPath);
+            bool stream = true;
+            if (ext == "")
             {
-                int i = 1;
+                ext = ".wav";
+                trackPath = trackPath + ext;
             }
-            selectedClip.name = bgmMusic.name;
-            bgmMusic = selectedClip;
+            if (ext is ".mod" or ".s3m" or ".it" or ".xm")
+            {
+                stream = false;
+            }
+            if (File.Exists(trackPath) && Plugin.getAudioType(ext) != AudioType.UNKNOWN)
+            {
+                WWW audioLoader = new(Plugin.FilePathToFileUrl(trackPath));
+                AudioClip selectedClip = audioLoader.GetAudioClip(false, stream, Plugin.getAudioType(ext));
+                while (!(selectedClip.loadState == AudioDataLoadState.Loaded))
+                {
+                    int i = 1;
+                }
+                selectedClip.name = bgmMusic.name;
+                bgmMusic = selectedClip;
+            }
         }
     }
 }
